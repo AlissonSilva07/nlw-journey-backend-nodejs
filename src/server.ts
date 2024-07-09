1dec84233132673da84cba5a33d184a53a207d8a
Alisson Rego <alissondroid07@gmail.com>
2024-07-09 13:09:19 -0300

    commit inicial e aula 1

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import fastify from 'fastify'
+import cors from '@fastify/cors'
+import { serializerCompiler, validatorCompiler } from 'fastify-type-provider-zod';
+import { createTrip } from './routes/create-trip';
+
+const app = fastify()
+
+app.register(cors, {
+    origin: '*'
+})
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.register(createTrip)
+
+app.listen({ port: 3333 }).then(() => {
+    console.log('Server running on port 3333.')
+})
