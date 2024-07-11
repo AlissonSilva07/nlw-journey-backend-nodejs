ead54e18f90cea40d9e0335f3b7486aa9ef499e1
Alisson Rego <alissondroid07@gmail.com>
2024-07-11 12:41:10 -0300

    feat: aula 02 e aula 03 + correções

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,47 @@
-import fastify from 'fastify'
+import fastify from "fastify";
 import cors from '@fastify/cors'
-import { serializerCompiler, validatorCompiler } from 'fastify-type-provider-zod';
-import { createTrip } from './routes/create-trip';
+import { createTrip } from "./routes/create-trip";
+import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
+import { confirmTrip } from "./routes/confirm-trip";
+import { confirmParticipant } from "./routes/confirm-participant";
+import { createActivity } from "./routes/create-activity";
+import { getActivity } from "./routes/get-activities";
+import { createLink } from "./routes/create-link";
+import { getLinks } from "./routes/get-links";
+import { getParticipants } from "./routes/get-participants";
+import { createInvite } from "./routes/create-invite";
+import { updateTrip } from "./routes/update-trip";
+import { getTripDetails } from "./routes/get-trip-details";
+import { getParticipant } from "./routes/get-participant";
+import { errorHandler } from "./error-handler";
+import { env } from "./env";
+import { removeParticipantFromTrip } from "./routes/remove-participant-from-trip";
 
 const app = fastify()
 
 app.register(cors, {
-    origin: '*'
+  origin: env.WEB_BASE_URL,
 })
 
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
-app.register(createTrip)
+app.setErrorHandler(errorHandler)
 
-app.listen({ port: 3333 }).then(() => {
-    console.log('Server running on port 3333.')
+app.register(createTrip)
+app.register(updateTrip)
+app.register(confirmTrip)
+app.register(getTripDetails)
+app.register(confirmParticipant)
+app.register(getParticipants)
+app.register(getParticipant)
+app.register(removeParticipantFromTrip)
+app.register(createActivity)
+app.register(getActivity)
+app.register(createLink)
+app.register(getLinks)
+app.register(createInvite)
+
+app.listen({ port: env.PORT }, () => {
+  console.log("Server running on port 3333.")
 })
